c38cf432909e899e895b0a0a58a819e8a9701acd
maximLomako <maxim.lomako@icloud.com>
2020-11-16 17:13:45 +0300

    feat: add change size func

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 let numOfCellsinRow = 4;
-// пофиксить рендер
-let getCorrectArray = (size) => {
-  let numbers = [].fill("");
-  numbers[size ** 2 - 1] = "";
-  numbers.fill("");
-  numbers = numbers.map((el, ind) => ind);
-
-  let shuffle = (array, repeat) => {
-    for (let i = 0; i <= repeat; i += 1) {
-      array.sort(() => Math.random() - 0.5);
-    }
-  };
-  shuffle(numbers, size ** 2);
-
-  const field = [];
-  for (let i = 0; i < size; i += 1) {
-    field[i] = [];
-    for (let j = 0; j < size; j += 1) {
-      field[i][j] = numbers.pop();
-    }
-  }
-  return field.flat();
-};
-
-const arrOfNum = getCorrectArray(numOfCellsinRow);
-
-const createField = () => {
+const createPage = () => {
   const body = document.querySelector('body');
-  const numbers = arrOfNum;
-  console.log(numbers);
 
-  body.insertAdjacentHTML('afterbegin',
-    `
+  let getCorrectArray = (size) => {
+    let numbers = [].fill("");
+    numbers[size ** 2 - 1] = "";
+    numbers.fill("");
+    numbers = numbers.map((el, ind) => ind);
+
+    let shuffle = (array, repeat) => {
+      for (let i = 0; i <= repeat; i += 1) {
+        array.sort(() => Math.random() - 0.5);
+      }
+    };
+    shuffle(numbers, size ** 2);
+
+    const field = [];
+    for (let i = 0; i < size; i += 1) {
+      field[i] = [];
+      for (let j = 0; j < size; j += 1) {
+        field[i][j] = numbers.pop();
+      }
+    }
+    return field.flat();
+  };
+
+  let arrOfNum = getCorrectArray(numOfCellsinRow);
+
+  const createField = () => {
+
+    const numbers = arrOfNum;
+
+    body.insertAdjacentHTML('afterbegin',
+      `
 <div class="container">
   <div class="indicators">
     <div class="counter">000</div>
@@ -43,137 +44,242 @@ const createField = () => {
   <div class="field"></div>
 </div>`);
 
-  const field = document.querySelector('.field');
-  numbers.map((c, i) => {
-    field.insertAdjacentHTML('beforeend', `<div class="cell" >${c === 0 ? '' : c}</div>`);
-  });
-}
-createField();
+    const field = document.querySelector('.field');
+    numbers.map((c, i) => {
+      field.insertAdjacentHTML('beforeend', `<div class="cell" draggable="true">${c === 0 ? '' : c}</div>`);
+    });
+  }
+  createField();
 
+  const createModalWindow = () => {
+    body.insertAdjacentHTML('afterbegin',
+      `<div class="modal">
+              <div class="modal__dialog">
+                <div class="modal__item">New game</div>
+                <div class="modal__item">Save game</div>
+                <div class="modal__item">Change size</div>
+                <div class="modal__item">Best scores</div>
+                <div class="modal__item">Sound</div>
+                <div class="close">
+                <img src="./assets/icons/close.svg" alt="close-icon">
+                </div>
+              </div>
+             </div>`
+    );
+  }
+  createModalWindow();
 //global variable
-const container = document.querySelector('.container'),
-  field = document.querySelector('.field'),
-  cell = document.querySelectorAll('.cell'),
-  counter = document.querySelector('.counter'),
-  timer = document.querySelector('.timer');
+  const container = document.querySelector('.container'),
+    field = document.querySelector('.field'),
+    cell = document.querySelectorAll('.cell'),
+    counter = document.querySelector('.counter'),
+    timer = document.querySelector('.timer'),
+    gear = document.querySelector('.gear'),
+    gearIcon = document.querySelector('.gear img'),
+    modal = document.querySelector('.modal'),
+    modalDialog = document.querySelector('.modal__dialog'),
+    close = document.querySelector('.close'),
+    modalItems = document.querySelectorAll('.modal__item'),
+    fieldSizeValue = ['3x3', '4x4', '5x5', '6x6', '7x7', '8x8'];
+
 // let initArrLength = 15;
-let fieldWidth = '';
-let cellWidth = '';
-let cellHeight = '';
-const cells = [];
-let empty = {};
-let counterValue = 0;
+  let fieldWidth = '';
+  let cellWidth = '';
+  let cellHeight = '';
+  let fontSize = '';
+  const cells = [];
+  let empty = {};
+  let counterValue = 0;
 
-const initGame = () => {
-  fieldWidth = window.getComputedStyle(field).getPropertyValue('width');
-  cellWidth = parseInt(fieldWidth, 10) / numOfCellsinRow + 'px';
-  cellHeight = cellWidth;
-  field.style.height = fieldWidth;
+  const initGame = () => {
+    fieldWidth = window.getComputedStyle(field).getPropertyValue('width');
+    cellWidth = parseInt(fieldWidth, 10) / numOfCellsinRow + 'px';
+    cellHeight = cellWidth;
+    field.style.height = fieldWidth;
 
-  cell.forEach((c, i) => {
-      c.style.width = parseInt(fieldWidth, 10) / numOfCellsinRow + 'px';
-      c.style.height = parseInt(fieldWidth, 10) / numOfCellsinRow + 'px';
+    cell.forEach((c, i) => {
+        c.style.width = parseInt(fieldWidth, 10) / numOfCellsinRow + 'px';
+        c.style.height = parseInt(fieldWidth, 10) / numOfCellsinRow + 'px';
+        c.style.fontSize = '5vw';
 
-      const left = i % numOfCellsinRow;
-      const top = (i - left) / numOfCellsinRow;
+        const left = i % numOfCellsinRow;
+        const top = (i - left) / numOfCellsinRow;
 
-      if (arrOfNum[i] === 0) {
-        empty = {
-          value: 0,
-          top: top,
-          left: left,
-          element: null
+        if (arrOfNum[i] === 0) {
+          empty = {
+            value: 0,
+            top: top,
+            left: left,
+            element: null
+          }
+          cells.push({
+            value: 0,
+            top: top,
+            left: left,
+            element: null
+          })
+          c.style.visibility = 'hidden';
+
+        } else {
+          cells.push({ // закидываем в массив объектов все наши ячейки
+            value: +cell[i].textContent, // то, что написано в ячейке
+            left: left,
+            top: top,
+            element: cell[i]
+          });
         }
-        cells.push({
-          value: 0,
-          top: top,
-          left: left,
-          element: null
+
+        cell[i].style.top = `${top * parseInt(cellHeight, 10)}px`; // присваеваем расчетные значения в стили ячеек
+        cell[i].style.left = `${left * parseInt(cellWidth, 10)}px`;
+
+        //drag
+        const dragStart = () => {
+          setTimeout(() => {
+            c.classList.toggle('hide');
+          }, 0);
+        }
+        const dragEnd = () => {
+          c.classList.toggle('hide');
+          move(i);
+        }
+
+        const dragOver = () => {
+          console.log('over');
+        }
+        const dragEnter = () => {
+          console.log('enter');
+        }
+
+
+        cell[i].addEventListener('click', () => {
+          move(i);
         })
-        c.style.visibility = 'hidden';
 
-      } else {
-        cells.push({ // закидываем в массив объектов все наши ячейки
-          value: +cell[i].textContent, // то, что написано в ячейке
-          left: left,
-          top: top,
-          element: cell[i]
-        });
+        cell[i].addEventListener('dragstart', dragStart);
+        cell[i].addEventListener('dragend', dragEnd);
+
       }
+    )
+  }
+  initGame();
 
-      cell[i].style.top = `${top * parseInt(cellHeight, 10)}px`; // присваеваем расчетные значения в стили ячеек
-      cell[i].style.left = `${left * parseInt(cellWidth, 10)}px`;
 
-      cell[i].addEventListener('click', () => {
-        move(i);
+  const move = index => {
 
-      })
+    const cell = cells[index]; // взяли данные о ячейке из массива объектов
+    const leftDiff = Math.abs(empty.left - cell.left); // сравниваем значения по модулю
+    const topDiff = Math.abs(empty.top - cell.top); // сравниваем значения по модулю
+    if (leftDiff + topDiff > 1) {
+      return;
     }
-  )
-}
-initGame();
 
+    cell.element.style.top = `${empty.top * parseInt(cellHeight, 10)}px`; // по клику присваеваем значения пустой ячейки нашей ячейке
+    cell.element.style.left = `${empty.left * parseInt(cellWidth, 10)}px`; // по клику присваеваем значения пустой ячейки нашей ячейке
 
-const move = index => {
+    const emptyLeft = empty.left; // промежуточная переменная с координатами пустой ячейки
+    const emptyTop = empty.top; // промежуточная переменная с координатами пустой ячейки
 
-  const cell = cells[index]; // взяли данные о ячейке из массива объектов
-  const leftDiff = Math.abs(empty.left - cell.left); // сравниваем значения по модулю
-  const topDiff = Math.abs(empty.top - cell.top); // сравниваем значения по модулю
-  if (leftDiff + topDiff > 1) {
-    return;
+    empty.left = cell.left; // присваеваем нашей пустой ячейке координаты нашей ячеки по которой мы кликнули
+    empty.top = cell.top; // присваеваем нашей пустой ячейке координаты нашей ячеки по которой мы кликнули
+
+    cell.left = emptyLeft;
+    cell.top = emptyTop; // записываем в координаты яейки временные координаты пустой ячнйки
+
+    countMoves();
+
+    let cellsWithoutEmpty = cells.filter((element) => {
+      return element.value !== 0;
+    });
+
+    const isFinished = cellsWithoutEmpty.every(cell => {
+      return cell.value === (cell.top * numOfCellsinRow + cell.left) + 1;
+    })
+
+    if (isFinished) {
+      console.log('You win!');
+    }
   }
 
-  cell.element.style.top = `${empty.top * parseInt(cellHeight, 10)}px`; // по клику присваеваем значения пустой ячейки нашей ячейке
-  cell.element.style.left = `${empty.left * parseInt(cellWidth, 10)}px`; // по клику присваеваем значения пустой ячейки нашей ячейке
 
-  const emptyLeft = empty.left; // промежуточная переменная с координатами пустой ячейки
-  const emptyTop = empty.top; // промежуточная переменная с координатами пустой ячейки
+  const countMoves = () => {
+    counterValue = counterValue + 1;
+    if (counterValue < 10) {
+      counter.textContent = '00' + counterValue;
+    } else if (counterValue < 100) {
+      counter.textContent = '0' + counterValue;
+    } else {
+      counter.textContent = counterValue;
+    }
+  }
 
-  empty.left = cell.left; // присваеваем нашей пустой ячейке координаты нашей ячеки по которой мы кликнули
-  empty.top = cell.top; // присваеваем нашей пустой ячейке координаты нашей ячеки по которой мы кликнули
+  timer.innerHTML = '00:00'
+  let milliseconds = 0;
+  const startTimer = () => {
+    if (counterValue > 0) {
+      milliseconds += 10;
+      let dateTimer = new Date(milliseconds);
+      timer.innerHTML =
+        ('0' + dateTimer.getUTCMinutes()).slice(-2) + ':' +
+        ('0' + dateTimer.getUTCSeconds()).slice(-2)
+    }
+  }
+  setInterval(startTimer, 10);
 
-  cell.left = emptyLeft;
-  cell.top = emptyTop; // записываем в координаты яейки временные координаты пустой ячнйки
 
-  countMoves();
+  const toggleModal = () => {
+    modal.classList.toggle('modal__active')
+    gear.classList.toggle('active')
+  }
 
-  let cellsWithoutEmpty = cells.filter((element) => {
-    return element.value !== 0;
-  });
+//start new game
+  const startGame = () => {
+    modal.classList.toggle('modal__active');
+    modal.remove();
+    container.remove();
+    createPage();
+  }
 
-  const isFinished = cellsWithoutEmpty.every(cell => {
-    return cell.value === (cell.top * numOfCellsinRow + cell.left) + 1;
+  //change size
+  const changeFieldSize = () => {
+    modalDialog.innerHTML = '';
+    fieldSizeValue.map(s => {
+      modalDialog.insertAdjacentHTML('beforeend',
+        `<span class='field__size'>${s}</span>`)
+    });
+
+    modalDialog.addEventListener('click', (e) => {
+      if (e.target.classList.contains('field__size')) {
+        modal.classList.toggle('modal__active');
+        numOfCellsinRow = +e.target.textContent[0]
+        modal.remove();
+        container.remove();
+        createPage();
+      }
+    })
+
+
+
+  }
+
+  modalItems[0].addEventListener('click', startGame);
+  modalItems[2].addEventListener('click', changeFieldSize);
+  gearIcon.addEventListener('click', toggleModal);
+  close.addEventListener('click', toggleModal);
+
+  document.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.classList.toggle('modal__active')
+      gear.classList.toggle('active')
+    }
   })
 
-  if (isFinished) {
-    console.log('You win!');
-  }
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape') {
+      modal.classList.remove('modal__active')
+      gear.classList.toggle('active')
+    }
+  })
+
+  window.addEventListener("resize", initGame);
 }
-
-
-const countMoves = () => {
-  counterValue = counterValue + 1;
-  if (counterValue < 10) {
-    counter.textContent = '00' + counterValue;
-  } else if (counterValue < 100) {
-    counter.textContent = '0' + counterValue;
-  } else {
-    counter.textContent = counterValue;
-  }
-}
-
-timer.innerHTML = '00:00'
-let milliseconds = 0;
-const startTimer = () => {
-  if (counterValue > 0) {
-    milliseconds += 10;
-    let dateTimer = new Date(milliseconds);
-    timer.innerHTML =
-      ('0' + dateTimer.getUTCMinutes()).slice(-2) + ':' +
-      ('0' + dateTimer.getUTCSeconds()).slice(-2)
-  }
-}
-setInterval(startTimer, 10)
-
-
-window.addEventListener("resize", initGame);
+createPage();
